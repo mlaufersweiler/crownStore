0a2c482a1fbbbcd006dca6aae1be978f1a4f0e60
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook.local>
2019-10-22 09:56:16 -0400

    add firebasae authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.components";
+import { auth } from "./firebase/firebase.utils";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAV5vDnLaxfq1G-BAM1T_aidGTiSNEX_mE",
+  authDomain: "crwn-db-a8bc1.firebaseapp.com",
+  databaseURL: "https://crwn-db-a8bc1.firebaseio.com",
+  projectId: "crwn-db-a8bc1",
+  storageBucket: "crwn-db-a8bc1.appspot.com",
+  messagingSenderId: "1022894175808",
+  appId: "1:1022894175808:web:6b31f624912d88ba5d91a5",
+  measurementId: "G-F9NXT30Z4Y"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ propmt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
